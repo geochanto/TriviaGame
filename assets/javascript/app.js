1e3b31643da6225c1741ffde91b100af90b27f9f
Chanturidze <george.chanturidze@gmail.com>
2018-03-26 22:58:06 -0700

    add functions to cycle through questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,38 +3,82 @@ $(document).ready(function(){
 var correctAnswers = 0;
 var wrongAnswers = 0;
 var unanswered = 0;
+var next = 0;
 
 //JSON Object with questions & correct answers
-var questions = {
-    question1: {
+var questionsArray = [
+    {
         questionText: "who?",
-        choices: {
-            answer1: "you",
-            answer2: "me",
-            answer3: "her",
-            answer4: "him"
-        },
+        choices: [
+            "you",
+            "me",
+            "her",
+            "him"
+        ],
         correct: "her"
     },
-    question2: {
+    {
         questionText: "what?",
-        choices: {
-            answer1: "this",
-            answer2: "that",
-            answer3: "it",
-            answer4: "those"
-        },
+        choices: [
+            "this",
+            "that",
+            "it",
+        ],
         correct: "this"
-    }
-}
+    },
+    {
+        questionText: "where?",
+        choices: [
+            "here",
+            "there",
+            "yonder",
+            "near"
 
+        ],
+        correct: "near"
+    }
+]
+
+var currentQuestion = questionsArray[0];
+var numChoices = currentQuestion.choices.length;
+
+//function to start the game
 function startGame() {
+    //reset answers on game start
     correctAnswers = 0;
     wrongAnswers = 0;
     unanswered = 0;
+    var currentQuestion = questionsArray[0];
+    
     console.log('started!');
+    $('#questionText').text(currentQuestion.questionText);
+    for (var i = 0; i<numChoices; i++) {
+        var choice = currentQuestion.choices[i];
+        $('#answers').append('<div class="choice" data-attribute='+ choice +'>'+ choice +'</div>');
+    }
+
+    //hide the intro on game start
+    $('#intro').hide();
+
+    //show the questions div on game start
+    $('#questions').show();
 }
 
+// for (var i = 0; i<questionsArray.length; i++) {
+$('.next').click(function() {
+    $('#answers').empty();
+    console.log('next clicked!');
+    next++;
+    currentQuestion = questionsArray[next];
+    numChoices = currentQuestion.choices.length;
+    $('#questionText').text(currentQuestion.questionText);
+    for (var i = 0; i<numChoices; i++) {
+        var choice = currentQuestion.choices[i];
+        $('#answers').append('<div class="choice" data-attribute='+ choice +'>'+ choice +'</div>');
+    }
+});
+
+//call start game function when user clicks start or restart
 $('.start-restart').click(function() {
 startGame();
 });
